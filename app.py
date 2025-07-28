c296bf1b29157adb2c25c5307179c77e301b53e8
Ryuhei-Chino3 <ryuhei.chino@qenest.com>
2025-07-28 10:50:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ if run_button:
 
     # ✅ 休日データ → Q〜AB列（17〜28列）
     for m in range(4, 16):
-        col_idx = 16 + (m - 4)  # 修正: 4月→17(Q), ..., 翌年3月→28(AB)
+        col_idx = 17 + (m - 4)  # 修正: 4月→17(Q), ..., 翌年3月→28(AB)
         col_letter = get_column_letter(col_idx)
         for i in range(48):
             ws_template[f"{col_letter}{4+i}"] = monthly_data['holiday'][m][i]
