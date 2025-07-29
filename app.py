1b2e15850d9264313806a535d26e96129afd0efe
Ryuhei-Chino3 <ryuhei.chino@qenest.com>
2025-07-29 14:30:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ if st.session_state.uploaded_files:
 
 # クリアボタン
 if st.button("アップロードファイルを一括クリア"):
-    st.session_state.uploaded_files = []
-    st.session_state["file_uploader"] = None
+    if "uploaded_files" in st.session_state:
+        del st.session_state.uploaded_files
     st.experimental_rerun()
 
 output_name = st.text_input("出力ファイル名（拡張子不要）", value="", help="例：cats_202406 ※必須")
